37b88089015b95cffd52d729d53b01df66b167c5
Wenderson Rodrigues Malheiros <wenderson.malheiros@gmail.com>
2019-05-27 22:18:18 -0300

    Adding validations

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const routes = express.Router()
 const authMiddleware = require('./app/middlewares/auth')
+const validate = require('express-validation')
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(authMiddleware)
 
@@ -14,13 +24,17 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads/', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads/', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.delete)
 
 /**
  * Purchase
  */
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
